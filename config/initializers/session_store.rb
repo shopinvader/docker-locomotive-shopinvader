33069012f34ab5e93977751df47476c9dbf85cd2
Sébastien BEAU <sebastien.beau@akretion.com>
2017-10-09 12:22:19 +0200

    [IMP] store cookies for 2 years and fix passing env store_asset_in_s3

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :mongoid_store
+Rails.application.config.session_store(
+  :mongoid_store,
+  expire_after: 2.years
+)
