ae8fe0a0e7704f12d11c072c14fd0c969a4eb842
Sébastien BEAU <sebastien.beau@akretion.com>
2018-11-13 17:10:01 +0100

    [FIX] fix gosu installation error "gpg: cannot open '/dev/tty': No such device or address"

--- install/gosu.sh
+++ install/gosu.sh
@@ -17,11 +17,11 @@ do
   then
     ks_options="--keyserver-options http-proxy=$http_proxy"
   fi
-  gpg --keyserver "$server" $ks_options --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 && break || :
+  gpg --no-tty --keyserver "$server" $ks_options --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 && break || :
 done
 curl -o /usr/local/bin/gosu -SL "https://github.com/tianon/gosu/releases/download/1.10/gosu-$(dpkg --print-architecture)"
 curl -o /usr/local/bin/gosu.asc -SL "https://github.com/tianon/gosu/releases/download/1.10/gosu-$(dpkg --print-architecture).asc"
-gpg --verify /usr/local/bin/gosu.asc
+gpg --verify --no-tty /usr/local/bin/gosu.asc
 rm -rf "$GNUPGHOME" /usr/local/bin/gosu.asc
 chmod +x /usr/local/bin/gosu
 # verify that the binary works
