1f9234544aa5ca0755e95bc97435be84d1da0e92
Sébastien BEAU <sebastien.beau@akretion.com>
2019-04-03 01:24:25 +0200

    [IMP] add monkey patch to avoid useless empty session in mongodb

--- shopinvader/config/initializers/session_store.rb
+++ shopinvader/config/initializers/session_store.rb
@@ -10,3 +10,25 @@ Rails.application.config.session_store(
   key: '_station_session',
   expire_after: 2.years,
 )
+
+# Varnish will drop all cookie session for get method
+# this mean that we will have a lot of request without any session cookie
+# generating a cookie even if the session is empty
+
+module ActionDispatch
+  module Session
+    class MongoStoreBase
+      alias_method :orig_write_session, :write_session
+
+      def write_session(req, sid, session_data, options)
+        if session_data.size > 0
+          orig_write_session(req, sid, session_data, options)
+        else
+          delete_session(req, sid, options)
+          'nosession'
+        end
+      end
+
+    end
+  end
+end
