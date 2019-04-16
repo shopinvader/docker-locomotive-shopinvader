ccbf4ae69bc2f4089231bbbd092e9225089e1ceb
Sébastien BEAU <sebastien.beau@akretion.com>
2019-04-16 17:14:41 +0200

    Automatic release v4.0.x-20190416

---
+++ bump.sh
@@ -0,0 +1,9 @@
+rm shopinvader/Gemfile.lock
+docky run bundle install
+git add shopinvader/Gemfile shopinvader/Gemfile.lock
+TAG="v4.0.x-`date +%Y%m%d`"
+MESSAGE="Automatic release $TAG"
+git commit -m "$MESSAGE"
+git tag -a "$TAG" -m "$MESSAGE"
+echo "bump done, check result the run"
+echo "git push origin master --tag"
