cf81dd2417effd9c9c47c97e85873afe4526dd9a
Sébastien BEAU <sebastien.beau@akretion.com>
2019-11-06 15:59:43 +0100

    refactor entrypoint to solve issue with docker volume

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 set -e
 
-# loco user will have the same uid as the user that own the directory log and tmp
-USER_ID=$(stat -c %u /usr/src/app/log)
-
-if [ $USER_ID -eq "0" ]
-then
-  echo "The owner of log volume can not be root user"
-  exit 1
-elif [ $USER_ID -ne $(stat -c %u /usr/src/app/tmp) ]
-then
-  echo "The owner of tmp and log file should be the same"
-  exit 1
-fi
+# allow to customize the UID of the loco user,
+# so we can share the same than the host's.
+# If no user id is set, we use 999
+USER_ID=${LOCAL_USER_ID:-999}
 
 echo "Starting with UID : $USER_ID"
 id -u loco &> /dev/null || useradd --shell /bin/bash -u $USER_ID -o -c "" -m loco
 
-echo "Changing permissions on /usr/src/app/public with UID : $USER_ID"
-chown -R $USER_ID /usr/src/app
+echo "Changing permissions on writable directory with UID : $USER_ID"
+chown -R $USER_ID /usr/src/app/log
+chown -R $USER_ID /usr/src/app/tmp
+chown -R $USER_ID /usr/src/app/public/sites
+chown -R $USER_ID /usr/src/app/public/uploaded_assets
 
 exec "$@"
