c74012a3f56d2d070ca17db18f1fcb01acca6912
Sébastien BEAU <sebastien.beau@akretion.com>
2022-11-02 08:54:12 +0100

    Fix build

--- install/shopinvader.sh
+++ install/shopinvader.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -eo pipefail
+set -euxo pipefail
 
 bundle install --without development
 mkdir -p tmp log
 chown 9999:9999 tmp log
-bundle exec rake assets:precompile
+# a fake SECRET KEY is needed to build asset :'(
+# https://github.com/rails/rails/issues/32947
+SECRET_KEY_BASE=1 RAILS_ENV=production bundle exec rake assets:precompile
 rm -rf log/* tmp/*
