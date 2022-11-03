ad166883539d53eed6e5690d5b0546304611147b
Sébastien BEAU <sebastien.beau@akretion.com>
2022-11-03 18:27:43 +0100

    Automatic release v4.0.x-20221103

--- bump.sh
+++ bump.sh
@@ -5,6 +5,6 @@ git add shopinvader/Gemfile shopinvader/Gemfile.lock
 TAG="v4.0.x-`date +%Y%m%d`"
 MESSAGE="Automatic release $TAG"
 git commit -m "$MESSAGE"
-git tag -a "$TAG" -m "$MESSAGE"
+git tag "$TAG"
 echo "bump done, check result the run"
 echo "git push origin master --tag"
