6c242c954952f768d1b5fe89a02c6494ec56d09b
Sébastien BEAU <sebastien.beau@akretion.com>
2024-10-15 14:56:15 +0200

    Fix wrong carrier wave configuration

--- shopinvader/config/initializers/carrierwave.rb
+++ shopinvader/config/initializers/carrierwave.rb
@@ -30,20 +30,10 @@ CarrierWave.configure do |config|
       config.aws_bucket       = ENV['S3_BUCKET']
       config.aws_acl          = 'public-read'
 
-      # Use a different endpoint (eg: another provider such as Exoscale)
-      if ENV['S3_ENDPOINT'].present?
-        config.aws_credentials[:endpoint] = ENV['S3_ENDPOINT']
-      end
 
-      # For some endpoint like minio you need to rewrite path 
-      if ENV['S3_PATH_STYLE'].present?
-        config.aws_credentials[:force_path_style] = ENV['S3_PATH_STYLE']
-      end
-    
       config.aws_attributes = {
         cache_control: ENV['S3_CACHE_CONTROL']
       }
-
 
       config.aws_credentials  = {
         access_key_id:      ENV['S3_KEY_ID'],
@@ -51,6 +41,15 @@ CarrierWave.configure do |config|
         region:             ENV['S3_BUCKET_REGION']
       }
 
+      # Use a different endpoint (eg: another provider such as Exoscale)
+      if ENV['S3_ENDPOINT'].present?
+        config.aws_credentials[:endpoint] = ENV['S3_ENDPOINT']
+      end
+
+      # For some endpoint like minio you need to rewrite path
+      if ENV['S3_PATH_STYLE'].present?
+        config.aws_credentials[:force_path_style] = ENV['S3_PATH_STYLE']
+      end
 
     end
 
